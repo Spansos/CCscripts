7a09da8ed1b484ee6dfd4f865acc9e50eff51f40
Spansos <92796536+Spansos@users.noreply.github.com>
2022-01-09 00:13:09 +0100

    added already made scripts

---
+++ scripts/cropFarmer.lua
@@ -0,0 +1,95 @@
+function select_item(item)
+    for i=1, 16 do
+        item_data = turtle.getItemDetail(i, false)
+        if(item_data ~= nil and item_data.name == item) then
+            turtle.select(i)
+            break
+        end
+    end
+end
+
+function return_to_chest()
+    while(true) do
+        while(not turtle.detect()) do
+                turtle.forward()
+        end
+        is_block, block_data = turtle.inspect()
+        if(block_data.name == "minecraft:chest") then
+            turtle.turnLeft()
+            turtle.turnLeft()
+            break
+        end
+        turtle.turnLeft()
+    end
+end
+
+function action()
+    is_block, block_data = turtle.inspectDown()
+    if((not is_block) or ((block_data.name == "minecraft:wheat") and (block_data.state.age == 7))) then
+        turtle.digDown()
+        select_item("minecraft:wheat_seeds")
+        turtle.placeDown()
+    end
+end
+
+function go_over()
+    n = 1
+    while(not turtle.detect()) do
+        while(not turtle.detect()) do
+            action()
+            turtle.forward()
+        end
+        if(n%2 == 1) then
+            action()
+            turtle.turnRight()
+            turtle.forward()
+            turtle.turnRight()
+        else
+            action()
+            turtle.turnLeft()
+            turtle.forward()
+            turtle.turnLeft()
+        end
+        n = n + 1
+    end
+end
+
+function wait(n)
+    for i=1, n do
+        os.sleep(1)
+        print(string.format("%d/%d", i, n))
+    end
+end
+
+function dump()
+    fuels = {"minecraft:charcoal", "minecraft:coal"}
+    turtle.turnLeft()
+    turtle.turnLeft()
+    for i=1, 16 do
+        item_data = turtle.getItemDetail(i, false)
+        turtle.select(i)
+        if(item_data and fuels[item_data.name]) then
+            turtle.dropUp()
+        else
+            turtle.drop()
+        end
+    end
+    turtle.turnLeft()
+    turtle.turnLeft()
+end
+
+function fuel()
+    turtle.select(1)
+    while(turtle.getFuelLevel() < 3000) do
+        turtle.suckUp()
+        turtle.refuel()
+    end
+end
+
+while(true) do
+    return_to_chest()
+    dump()
+    wait(300)
+    fuel()
+    go_over()
+end
---
+++ scripts/treeFarmer.lua
@@ -0,0 +1,121 @@
+function move()
+    is_block, block_data = turtle.inspect()
+    if(is_block and block_data.name == "minecraft:oak_leaves") then
+        turtle.dig()
+    end
+    r, str = turtle.forward()
+    print(str)
+    return r
+end
+
+function go_back()
+    repeat
+        is_block, block_data = turtle.inspectDown()
+        if(block_data.name ~= "minecraft:cobblestone_slab" and block_data.name ~= "minecraft:cobblestone" and block_data.name ~= "minecraft:dirt" and block_data.name ~= "minecraft:grass_block") then
+            turtle.digDown()
+            turtle.down()
+        end
+    until(block_data.name == "minecraft:cobblestone_slab" or block_data.name == "minecraft:cobblestone" or block_data.name == "minecraft:dirt" or block_data.name == "minecraft:grass_block")
+    while(true) do
+        is_block, block_data = turtle.inspectDown()
+        if(block_data.name == "minecraft:cobblestone" or block_data.name == "minecraft:cobblestone_slab") then
+            break
+        else
+            if(not move()) then
+                turtle.turnRight()
+            end
+        end
+    end
+    repeat
+        if(not move()) then
+            turtle.turnRight()
+        end
+        is_block, block_data = turtle.inspectDown()
+        if(not (block_data.name == "minecraft:cobblestone" or block_data.name == "minecraft:cobblestone_slab")) then
+            turtle.turnRight()
+            turtle.turnRight()
+            move()
+            turtle.turnLeft()
+        end
+        is_block, block_data = turtle.inspect()
+    until(block_data.name == "minecraft:chest")
+end
+
+function chop(right)
+    turtle.dig()
+    move()
+    repeat
+        turtle.digUp()
+        turtle.up()
+        is_block, block_data = turtle.inspectUp()
+    until(block_data.name == "minecraft:cobblestone")
+    repeat
+        turtle.down()
+        is_block, block_data = turtle.inspectDown()
+    until(block_data.name == "minecraft:dirt" or block_data.name == "minecraft:grass_block")
+    turtle.turnRight()
+    turtle.turnRight()
+    move()
+    turtle.turnRight()
+    turtle.turnRight()
+end
+
+function action()
+    is_block, block_data = turtle.inspectDown()
+    if(block_data.name == "minecraft:cobblestone_slab") then
+        turtle.turnRight()
+        is_block, block_data = turtle.inspect()
+        if(block_data.name == "minecraft:oak_logs") then
+            chop(true)
+        end
+        turtle.turnRight()
+        turtle.turnRight()
+        is_block, block_data = turtle.inspect()
+        if(block_data.name == "minecraft:oak_logs") then
+            chop(false)
+        end
+        turtle.turnRight()
+    end
+end
+
+function three_move()
+    r = true
+    for i=1, 3 do
+        if not move() then
+            r = false
+        end
+    end
+    return r
+end
+
+function over_all()
+    move()
+    action()
+    n = 1
+    while(true) do
+        is_block, block_data = turtle.inspectDown()
+        if(is_block.name == "minecraft:cobblestone") then
+            if(n%2 == 1) then
+                turtle.turnLeft()
+                move()
+                move()
+                move()
+                turtle.turnLeft()
+            else
+                turtle.turnRight()
+                move()
+                move()
+                move()
+                turtle.turnRight()
+            end
+        else
+            move()
+            action()
+        end
+    end
+end
+
+while(true) do
+    go_back()
+    over_all()
+end
