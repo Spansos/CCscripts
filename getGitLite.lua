a0d481f89fa4f5c79859f0979bbbf5455c0be264
Spansos <92796536+Spansos@users.noreply.github.com>
2022-01-09 20:51:22 +0100

    Create getGitLite.lua

---
+++ getGitLite.lua
@@ -0,0 +1,9 @@
+args = {...}
+code = http.get("https://raw.githubusercontent.com/Spansos/CCscripts/main/"..args[1])
+out = io.open(args[2], "w")
+io.output(out)
+io.write(code.readAll()) 
+io.flush()
+out.close()
+code.close()
+io.close()
