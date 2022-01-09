4638abf5183309c898dd7db091b55716e00f8448
Spansos <92796536+Spansos@users.noreply.github.com>
2022-01-09 21:15:00 +0100

    did some things

--- getGit.lua
+++ getGit.lua
@@ -2,17 +2,16 @@ print("Checking turtle_info.txt...")
 if(fs.exists("/turtle_info.txt")) then
     print("Reading turtle_info.txt...")
     t_inf = {}
-    for l in lines("/turtle_info.txt") do
+    for l in io.lines("/turtle_info.txt") do
         func = string.gmatch(l, "([^:]+)")
-        key = f()
-        value = f()
-        print(k, v)
-        t_inf[k] = v
-
+        key = func()
+        value = func()
+        t_inf[key] = value
+    end
     print("Checking if script can be found on GitHub...")
     if(http.checkURL(string.format("https://raw.githubusercontent.com/Spansos/CCscripts/main/scripts/%s", t_inf.script))) then
         print("Downloading script...")
-        in_file = http.get("https://raw.githubusercontent.com/Spansos/CCscripts/main/scripts/%s", t_inf.script)
+        in_file = http.get("https://raw.githubusercontent.com/Spansos/CCscripts/main/scripts/" .. t_inf.script)
         print("Writing script...")
         out_file = io.open("script.lua", "w")
         io.output(out_file)
@@ -21,12 +20,12 @@ if(fs.exists("/turtle_info.txt")) then
         io.close(out_file)
         in_file.close()
         print("Running script...")
-        os.run("script.lua")
+        os.run({}, "script.lua")
     else
         print("Script can't be accessed.")
         print("Launching possibly older version...")
         if(fs.exists("script.lua")) then
-            os.run("script.lua")
+            os.run({}, "script.lua")
         else
             print("No offline script.")
             error()
