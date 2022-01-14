e1dbf717ef1193e20ee9d965e25d2e324716bcdc
Spansos <92796536+Spansos@users.noreply.github.com>
2022-01-14 14:29:58 +0100

    Update getGit.lua

--- getGit.lua
+++ getGit.lua
@@ -1,12 +1,9 @@
-print("Checking settings...")
-if(fs.exists("/.settings")) then
-    print("Reading settings.txt...")
-    script = settings.get("script")
-    print("---------------------------------------------------------"..script)
-    print("Checking if script can be found on GitHub...")
-    if(http.checkURL("https://raw.githubusercontent.com/Spansos/CCscripts/main/scripts/"..script)) then
-        print("Downloading script...")
-        in_file = http.get("https://raw.githubusercontent.com/Spansos/CCscripts/main/scripts/"..script)
+print("Getting script settings...")
+script = settings.get("script", false)
+if script then
+    print("Downloading from GitHub...")
+    in_file = http.get("https://raw.githubusercontent.com/Spansos/CCscripts/main/"..script)
+    if in_file then
         print("Writing script...")
         out_file = io.open("script.lua", "w")
         io.output(out_file)
@@ -17,7 +14,7 @@ if(fs.exists("/.settings")) then
         print("Running script...")
         os.run({}, "script.lua")
     else
-        print("Script can't be accessed.")
+        print("Script can't be found.")
         print("Launching possibly older version...")
         if(fs.exists("script.lua")) then
             os.run({}, "script.lua")
@@ -25,8 +22,7 @@ if(fs.exists("/.settings")) then
             print("No offline script.")
             error()
         end
-    end
 else
-    print("No options file")
+    print("No script option set.")
     error()
 end
