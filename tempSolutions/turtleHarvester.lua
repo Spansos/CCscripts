6db5822fd356f333b632401ef250179a866cf859
Spansos <92796536+Spansos@users.noreply.github.com>
2022-07-13 19:10:49 +0200

    Update turtleHarvester.lua

--- tempSolutions/turtleHarvester.lua
+++ tempSolutions/turtleHarvester.lua
@@ -1,9 +1,28 @@
-while true do
-    local bIs, tInfo = turtle.inspect()
-    if tInfo['name'] == 'computercraft:turtle_normal' then
-        turtle.dig()
-        turtle.dropDown()
-    else
-        os.sleep(1)
+local function try_empty()
+    for i=1, 16 do
+        turtle.select(i)
+        turtle.dropUp()
     end
+end
+
+local function inv_full()
+    for i=1, 16 do
+        if turtle.getItemCount(i) == 0 then
+            return false
+        end
+    end
+    return true
+end
+
+local function try_break()
+    if inv_full() then return end
+    local is_block, block_data = turtle.inspect()
+    if is_block and block_data['name'] == 'computercraft:turtle_normal' then
+        turtle.dig()
+    end
+end
+
+while true do
+    try_empty()
+    try_break()
 end
