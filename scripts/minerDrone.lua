58578255883ebecad6424ad9695ac966cf310d85
Spansos <92796536+Spansos@users.noreply.github.com>
2022-07-18 01:14:58 +0200

    Update minerDrone.lua

--- scripts/minerDrone.lua
+++ scripts/minerDrone.lua
@@ -10,13 +10,15 @@ end
 
 local function calc_pos_and_size(root, size, tot_n, n)
     local div1, div2 = get_divs(tot_n)
-    local base_size = {math.ceil(size[1]/div1), math.ceil(size[2]/div2)}
+    local base_size = {size[1]/div1, size[2]/div2}
     local grid_pos = {((n-1)%div1), math.floor((n-1)/div1)}
     local new_pos = {grid_pos[1]*base_size[1]+root[1], root[2], grid_pos[2]*base_size[2]+root[3]}
     local new_size = {
         math.min(math.abs(root[1]+size[1]-new_pos[1]), base_size[1]),
         math.min(math.abs(root[3]+size[2]-new_pos[3]), base_size[2])
     }
+    new_pos = {math.floor(new_pos[1]), new_pos[2], math.floor(new_pos[3])}
+    new_size = {math.ceil(new_size[1]), math.ceil(new_size[2])}
     return new_pos, new_size
 end
 
