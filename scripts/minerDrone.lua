5941ba681b9f41de987d17d25fb4dc6b14ba7e01
Spansos <92796536+Spansos@users.noreply.github.com>
2022-07-18 01:47:32 +0200

    Update minerDrone.lua

--- scripts/minerDrone.lua
+++ scripts/minerDrone.lua
@@ -264,7 +264,7 @@ local function mine(root_pos, mine_size, pos, dir_vec)
     while get_save()['mineY'] > END_Y and not inv_full() do
         local mineY = get_save()['mineY']
         moveto({root_pos[1], mineY, root_pos[3]}, pos, mineY, dir_vec)
-        turnto(dir_vec, {0, 1})
+        turnto(dir_vec, {1, 0})
         dig_down(pos, 3)
         mine_plane(mine_size, pos, dir_vec)
         local new_save = get_save()
