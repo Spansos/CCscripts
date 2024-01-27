28e2c483c0462b43cc28748de115ad0f898919e8
Spansos <92796536+Spansos@users.noreply.github.com>
2024-01-27 22:48:10 +0100

    code was so ugly i just had to change it

--- getGit.lua
+++ getGit.lua
@@ -1,29 +1,28 @@
 print("Getting script settings...")
 script = settings.get("script", false)
-if script then
-    print("Downloading from GitHub...")
-    in_file = http.get("https://raw.githubusercontent.com/Spansos/CCscripts/main/"..script)
-    if in_file then
-        print("Writing script...")
-        out_file = io.open("script.lua", "w")
-        io.output(out_file)
-        io.write(in_file.readAll())
-        io.flush()
-        io.close(out_file)
-        in_file.close()
-        print("Running script...")
-        os.run({}, "script.lua")
-    else
-        print("Script can't be found.")
-        print("Launching possibly older version...")
-        if(fs.exists("script.lua")) then
-            shell.run("script.lua")
-        else
-            print("No offline script.")
-            error()
-        end
-    end
-else
+if not script then
     print("No script option set.")
     error()
 end
+    
+print("Downloading from GitHub...")
+in_file = http.get("https://raw.githubusercontent.com/Spansos/CCscripts/main/"..script)
+if not in_file then
+    print("Script can't be found.")
+    print("Trying possibly older version...")
+    if not fs.exists("script.lua") then
+        print("No offline script.")
+        error()
+    end
+    shell.run("script.lua")
+end
+        
+print("Writing script...")
+out_file = io.open("script.lua", "w")
+io.output(out_file)
+io.write(in_file.readAll())
+io.flush()
+io.close(out_file)
+in_file.close()
+print("Running script...")
+os.run({}, "script.lua")
